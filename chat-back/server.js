ca0c68903408e4d5380fb3993da0bdc77d54d596
Julio Romero <jromeroacive@gmail.com>
2025-01-24 11:58:05 -0400

    Chat server-client

--- chat-back/server.js
+++ chat-back/server.js
@@ -13,10 +13,20 @@ const io = require('socket.io')(server,
 io.on('connection', (socket) =>{
     console.log('connection');
 
+ socket.on('sendChatToServer', (message) =>{
+    console.log(message);
+
+
+    //io.sockets.emit('sendChatToClient',  message);
+
+    socket.broadcast.emit('sendChatToClient',message);
+ })
+
+
     socket.on('disconnect', (socket) =>{
         console.log('Disconnect');
-    })
-})
+    });
+});
 
 server.listen(3000, () =>{
     console.log('Server is running');
